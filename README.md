31a2adb5932584180221ad4d8e96ce91229b2050
Maziar <Maziar.mod@gmail.com>
2023-01-30 13:55:44 -0500

    add Data Persistence with API Requests

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# Reading List (React App v1.0)
+# Reading List (React App v2.0)
 
 Users are going to be able to use our application to put together a list of books that they would like to read.
+
+Data Persistence with API Requests
+
+## How to run
+
+### Backend
+
+npm run server
+
+### Frontend
+
+npm start
 
 ## Resource
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newtitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newtitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -16,7 +30,9 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -24,14 +40,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+    const updatedBooks = [...books, response.data];
+
     setBooks(updatedBooks);
   };
 
